338bf3e0127fee9b9e117e8db3a75131e03831e0
Josep Castro <jcastro@ingeniamc.com>
2018-08-29 10:00:43 +0200

    added board and install script

---
+++ install_board.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+display_usage()
+{ 
+	echo -e "\nUsage:\n./install_board.sh <PATH TO PDK>\n" 
+}
+
+# if less than two arguments supplied, display usage 
+if [ $# -ne 1 ] 
+then 
+	display_usage
+	exit 1
+fi
+
+SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
+
+# copy starterware board
+cp $SCRIPTPATH/scr/am335x_coco.c $1/packages/ti/starterware/board/am335x/
+cp $SCRIPTPATH/scr/am335x_coco.h $1/packages/ti/starterware/board/am335x/
+cp $SCRIPTPATH/scr/am335x_coco_pinmux_data.c $1/packages/ti/starterware/board/am335x/
+
+# copy board
+cp -rf $SCRIPTPATH/scr/cocoAM335x $1/packages/ti/board/src/
