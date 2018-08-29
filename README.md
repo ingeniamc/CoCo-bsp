ac3f149c85ac764e6ff6586cf0c92f368f10f183
Josep Castro <jcastro@ingeniamc.com>
2018-08-29 11:59:10 +0200

    fixed install issues

--- README.md
+++ README.md
@@ -7,3 +7,10 @@ Run command:
 # find ./pdk_am335x_1_0_8/packages/ \( -name "*.mk" -o -name "*.bld" -o -name "*.h" -o -name "*.make" -o -name "*.c" -o -name "*.bat" -o -name "*.sh" \) \
 	-exec diff -Nau {} <PATH TO MODIFIED PDK>/{} \; > <PATH TO COCO BOARD REPO>/coco_board.patch
 ```
+
+## Apply the patch
+
+Run command:
+```
+# patch -p2 <PATH TO REPO>/coco_board.patch 
+```
--- install_board.sh
+++ install_board.sh
@@ -14,14 +14,17 @@ fi
 
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 
+echo "Copy new board files to path $1"
 # copy starterware board
-cp $SCRIPTPATH/scr/am335x_coco.c $1/packages/ti/starterware/board/am335x/
-cp $SCRIPTPATH/scr/am335x_coco.h $1/packages/ti/starterware/board/am335x/
-cp $SCRIPTPATH/scr/am335x_coco_pinmux_data.c $1/packages/ti/starterware/board/am335x/
+cp $SCRIPTPATH/src/am335x_coco.c $1/packages/ti/starterware/board/am335x/
+cp $SCRIPTPATH/src/am335x_coco.h $1/packages/ti/starterware/board/am335x/
+cp $SCRIPTPATH/src/am335x_coco_pinmux_data.c $1/packages/ti/starterware/board/am335x/
 
 # copy board
-cp -rf $SCRIPTPATH/scr/cocoAM335x $1/packages/ti/board/src/
+cp -rf $SCRIPTPATH/src/cocoAM335x $1/packages/ti/board/src/
 
+echo "Gonna build (path: <$1packages>"
 pushd $1/packages
+source ./pdksetupenv.sh
 make clean && make LIMIT_SOCS="am335x" starterware_libs board_lib spi csl gpio uart i2c icss-emac profiling osal pruss
 popd
